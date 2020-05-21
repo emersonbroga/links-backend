d5881f152eaa1c4ef4cf551c9f4ed08070f06001
Emerson Broga <emerson@emersonbroga.com>
2020-05-21 19:09:56 -0300

    Added some express configurations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   return res.json('Api running...');
