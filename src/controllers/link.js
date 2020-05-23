2ae426b48638494bb442ee9e97f907dbf0b0252f
Emerson Broga <emerson@emersonbroga.com>
2020-05-23 13:57:59 -0300

    LINKS Crud.

--- src/controllers/link.js
+++ src/controllers/link.js
@@ -10,6 +10,14 @@ router.get('/', async (req, res) => {
   return res.jsonOK(links);
 });
 
+router.get('/:id', async (req, res) => {
+  const accountId = req.accountId || 2;
+  const { id } = req.params;
+  const link = await Link.findOne({ where: { accountId, id } });
+  if (!link) return res.jsonNotFound();
+  return res.jsonOK(link);
+});
+
 router.post('/', async (req, res) => {
   const accountId = req.accountId || 2;
   const { label, url, icon, isSocial, order } = req.body;
@@ -22,6 +30,33 @@ router.post('/', async (req, res) => {
     accountId,
   });
   return res.jsonOK(link);
+});
+
+router.put('/:id', async (req, res) => {
+  const accountId = req.accountId || 2;
+  const { body } = req;
+  const { id } = req.params;
+  const fields = ['label', 'icon', 'url', 'isSocial', 'order'];
+
+  const link = await Link.findOne({ where: { accountId, id } });
+  if (!link) return res.jsonNotFound();
+
+  fields.map((fieldName) => {
+    const newValue = body[fieldName];
+    if (newValue) link[fieldName] = newValue;
+  });
+  await link.save();
+  return res.jsonOK(link);
+});
+
+router.delete('/:id', async (req, res) => {
+  const accountId = req.accountId || 2;
+  const { id } = req.params;
+
+  const link = await Link.findOne({ where: { accountId, id } });
+  if (!link) return res.jsonNotFound();
+  await link.destroy();
+  return res.jsonOK();
 });
 
 module.exports = router;
