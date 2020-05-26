10fb86728a3e567d916b2f82a92edd3d4cd6103d
Emerson Broga <emerson@emersonbroga.com>
2020-05-26 07:54:16 -0300

    Excluded auth paths from JWT.

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,6 +1,12 @@
 const { verifyJwt } = require('../helpers/jwt');
 
 const checkJwt = (req, res, next) => {
+  const { url: path } = req;
+
+  const excludedPaths = ['/auth/sign-in', '/auth/sign-up'];
+  const isExcluded = !!excludedPaths.find((p) => p.startsWith(path));
+  if (isExcluded) return next();
+
   let token = req.headers['authorization'];
   token = token ? token.slice(7, token.length) : null;
   if (!token) {
