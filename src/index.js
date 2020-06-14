06e9350e65928b8ac8342f005fbd3d29b0c9b5d6
Emerson Broga <emerson@emersonbroga.com>
2020-06-14 08:33:06 -0300

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const db = require('./models');
 const response = require('./middlewares/response');
 const checkJwt = require('./middlewares/jwt');
@@ -8,6 +9,7 @@ const linkController = require('./controllers/link');
 
 const app = express();
 
+app.use(cors());
 app.use(response);
 app.use(checkJwt);
 
